881dea1f15c183fd1267e449a00880c7335b97bd
teukusem <semteuku02@gmail.com>
2024-09-06 01:30:09 +0700

    feature/ pwa for mobile

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+import withPWA from 'next-pwa';
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table"],
 };
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+export default pwaConfig(nextConfig);
